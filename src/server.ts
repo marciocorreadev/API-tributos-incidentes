31b33b6a65c68a92214b49249919ca5eaab5c648
Márcio Corrêa <msc@outlook.com.br>
2021-01-23 23:14:47 -0300

    Inicionado TypeScript e ts-node-dev

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express, { request } from 'express';
+
+const app = express();
+
+app.get('/index', (request, response) => {
+    return response.send('API que retorna dados tributários da Lei da Transparência – Lei 12.741/2012.')
+})
+
+app.listen(3000)
