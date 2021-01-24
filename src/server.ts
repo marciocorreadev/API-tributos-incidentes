292dc507af6703e7e31f024006b43de81add6087
Márcio Corrêa <msc@outlook.com.br>
2021-01-23 23:59:39 -0300

    adicionado es lint

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import express, { request } from 'express';
+import express from 'express';
+
 const app = express();
-
 app.get('/index', (request, response) => {
-    return response.send('API que retorna dados tributários da Lei da Transparência – Lei 12.741/2012.')
-})
+  response.send('API que retorna dados tributários da Lei datributários da Lei datributários da Lei datributários da Lei datributários da Lei datributários da Lei datributários da Lei da Transparência – Lei 12.741/2012.');
+});
 
-app.listen(3000)
+app.listen(3000);
